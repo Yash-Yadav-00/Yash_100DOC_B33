ad327661645ccc08cbd981a32f1805c57b4affc4
Yash Yadav <yashyadav4651@gmail.com>
2025-11-01 22:54:36 +0530

    Refactor sum of digits program for clarity

--- Day19_Q2.c
+++ Day19_Q2.c
@@ -1,4 +1,4 @@
-//Q38: Write a program to find the sum of digits of a number.
+// Q38: Write a program to find the sum of digits of a number.
 
 /*
 Sample Test Cases:
@@ -15,19 +15,21 @@ Output 2:
 */
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
-    int i,remainder,sum = 0;                          //Declare variables(we will initialize sum as 0 otherwise it will have garbage value)
+    int i, remainder, sum = 0; // Declare variables(we will initialize sum as 0 otherwise it will have garbage value)
 
-    printf("Enter a Number: ");                   //Take Input From User
-    scanf("%d",&i);
+    printf("Enter a Number: "); // Take Input From User
+    scanf("%d", &i);
 
-    for(; i > 0;i = i/10){                     //divide by 10 to get rid of the last value 
-    remainder = i % 10;                       //gets the last digit
-    sum = sum + remainder;                    //Add to the sum  
+    for (; i > 0;)
+    {
+        remainder = i % 10;    // gets the last digit
+        i = i / 10;            // divide by 10 to get rid of the last value
+        sum = sum + remainder; // Add to the sum
     }
-    printf("%d",sum);
-    
+    printf("%d", sum);
 
     return 0;
 }
