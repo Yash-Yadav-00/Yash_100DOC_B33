b0b66e59ef6916a0c879a0ceb8c8e87ee41dcc3d
Yash Yadav <yashyadav4651@gmail.com>
2025-11-04 19:50:36 +0530

    Implement element insertion in an array
    Fixed input handling and added functionality to insert an element at a specified position in the array.

--- Day34_Q1.c
+++ Day34_Q1.c
@@ -10,22 +10,36 @@
 // 10 20 15 30 40
 
 // */
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main()
+{
 
-    int a;
+    int a, b, n, pos, j;
 
-    printf("Enter No. of Elements: ");
-    scnaf("%d",a);
-    int arr[a];
+    printf("Enter No. of Elements: "); // Ask Number of elements
+    scanf("%d", &a);
+    int arr[a + 1]; // Increase Size of array by 1
 
-    printf("Enter Your Elements: ");
-
-    for(int i =0;i<a;i++){
-scanf("%d",arr[i]);
+    printf("Enter Elements: "); // Take Input Of Elements
+    for (int i = 0; i < a; i++)
+    {
+        scanf("%d", &arr[i]);
     }
-    
 
+    printf("Enter the Position and the Element: "); // Input Position and Elements
+    scanf("%d%d", &pos, &n);
+
+    for (j = a; j >= pos; j--) // start the loop from the last position until we reach position
+    {
+        arr[j] = arr[j - 1]; // shift all the elements to right after position
+    }
+
+    arr[pos - 1] = n; // assign thevalue given by user at the given position
+
+    for (int l = 0; l < a + 1; l++) // Print Array with the increased element
+    {
+        printf("%d ", arr[l]);
+    }
 
     return 0;
 }
