4058987b6387cff7ef450904a11deb152febc73f
Yash Yadav <yashyadav4651@gmail.com>
2025-11-04 18:18:04 +0530

    Add binary conversion program
    Implemented a program to convert a decimal number to its binary representation.

--- Day16_Q1.c
+++ Day16_Q1.c
@@ -1,19 +1,46 @@
-//Logic Doubt
+// Q31: Write a program to take a number as input and print its equivalent binary representation.
+
+// /*
+// Sample Test Cases:
+// Input 1:
+// 10
+// Output 1:
+// 1010
+
+// Input 2:
+// 7
+// Output 2:
+// 111
+
+// */
 #include <stdio.h>
 
-int main(){
+int main()
+{
+    int num;
+    int binary = 0;
+    int remainder;
+    int place = 1;
 
-   int a,i,rem;
+    printf("Enter a number: ");
+    scanf("%d", &num);
 
-   printf("Enter a Number: ");
-   scanf("%d",&a);
+    // Handle 0 explicitly
+    if (num == 0)
+    {
+        printf("Binary: 0\n");
+        return 0;
+    }
 
-   while(a > 0;){
+    while (num > 0)
+    {
+        remainder = num % 2;                 // get binary digit
+        binary = binary + remainder * place; // build binary number
+        num = num / 2;                       // divide by 2
+        place = place * 10;                  // increase place value (1,10,100,...)
+    }
 
-    a % 2 = rem;
-    
-   }
+    printf("Binary: %d\n", binary);
 
     return 0;
-
 }
